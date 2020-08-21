515813d963b6b0b07a7c857ffaeb4a0fafe44aa6
yeguoqiang@netposa.com <yeguoqiang@netposa.com>
2020-08-21 10:33:31 +0800

    1.添加日志打印框架log4j 2.mybatis的CRUD操作

--- MybatisBase/src/main/java/org/mybatis/example/dao/BlogDao.java
+++ MybatisBase/src/main/java/org/mybatis/example/dao/BlogDao.java
@@ -3,7 +3,17 @@ package org.mybatis.example.dao;
 import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.example.entity.Blog;
 
+import java.util.List;
+
 @Mapper
 public interface BlogDao {
     Blog findBlog(int id);
+
+    List<Blog> findAll();
+
+    void addBlog(Blog blog);
+
+    void updateBlog(Blog blog);
+
+    void deleteById(int id);
 }
--- MybatisBase/src/test/java/com/example/test/MybatisTest.java
+++ MybatisBase/src/test/java/com/example/test/MybatisTest.java
@@ -10,17 +10,68 @@ import org.mybatis.example.entity.Blog;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class MybatisTest {
 
+    InputStream is;
+    SqlSession sqlSession;
+    BlogDao blogDao;
+
     @Test
     public void testFind() throws IOException {
-        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+        is = Resources.getResourceAsStream("mybatis-config.xml");
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        BlogDao blogDao = sqlSession.getMapper(BlogDao.class);
-        Blog blog = blogDao.findBlog(1);
-        System.out.println(blog);
+        sqlSession = sqlSessionFactory.openSession();
+        blogDao = sqlSession.getMapper(BlogDao.class);
+        //query
+        List<Blog> blogs = blogDao.findAll();
+        for (Blog blog : blogs) {
+            System.out.println(blog);
+        }
+        close();
+    }
+
+    @Test
+    public void testAdd() throws IOException {
+        init();
+        //insert
+        Blog addBlog = new Blog();
+        addBlog.setName("典韦");
+        blogDao.addBlog(addBlog);
+        sqlSession.commit();
+        close();
+    }
+
+    @Test
+    public void testDelete() throws IOException {
+        init();
+        //delete
+        blogDao.deleteById(13);
+        sqlSession.commit();
+        close();
+    }
+
+    @Test
+    public void testUpdate() throws IOException {
+        init();
+        //update
+        Blog updateBlog = new Blog();
+        updateBlog.setId(2);
+        updateBlog.setName("韩信");
+        blogDao.updateBlog(updateBlog);
+        sqlSession.commit();
+        close();
+    }
+
+    private void init() throws IOException {
+        is = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+        sqlSession = sqlSessionFactory.openSession();
+        blogDao = sqlSession.getMapper(BlogDao.class);
+    }
+
+    private void close() throws IOException {
         sqlSession.close();
         is.close();
     }
